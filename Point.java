e105aa7c82c99ccf75003cf12b872217e4058603
Manfred <mtan00@stuy.edu>
2019-04-03 14:00:27 -0400

    Skeleton Commit

---
+++ Point.java
@@ -0,0 +1,13 @@
+/**
+  Represent a point, using Cartesian coordinates
+ */
+public class Point {
+    private double xcor;
+    private double ycor;
+
+    // constructor
+    public Point( double xcor, double ycor) {
+        this.xcor = xcor;
+        this.ycor = ycor;
+    }
+}
